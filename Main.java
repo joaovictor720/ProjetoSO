f116700dec8c9842351e30546878c6bcfb8c7553
joaovictor720 <silvajoao720@gmail.com>
2023-08-27 16:50:36 -0300

    setting up the enviroment

---
+++ Main.java
@@ -0,0 +1,8 @@
+import packagetest.Classe;
+
+public class Main {
+    public static void main(String[] args) {
+        Classe test = new Classe();
+        test.printHelloWorld();
+    }
+}
---
+++ packagetest/Classe.java
@@ -0,0 +1,7 @@
+package packagetest;
+
+public class Classe {
+    public void printHelloWorld() {
+        System.out.println("Hello from the class");
+    }
+}
